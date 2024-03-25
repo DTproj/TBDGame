70fe2e0ceca297f891745b5229f7e5aa2d865b68
DTproj <dorian.taradi@gmail.com>
2024-03-25 15:19:49 +0100

    Initial commit

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 8f;
+    public Vector3 offset;
+
+    private float newX;
+    private float newY;
+    private float newZ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) return;
+
+        newX = target.position.x + offset.x;
+        newY = target.position.y + offset.y;
+        newZ = target.position.z + offset.z;
+
+        Vector3 newPos = new Vector3(newX, newY, newZ);
+        Vector3 smoothedPos = Vector3.Lerp(target.position, newPos, smoothing * Time.deltaTime);
+        transform.position = smoothedPos;
+    }
+}
