11bd01dd595f70d3563c85f67e084a3c175d6f8f
DTproj <dorian.taradi@gmail.com>
2024-03-26 19:22:23 +0100

    some stuff from today idk

---
+++ Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour
+{
+    private Animator animator;
+    private bool isOpen = false;
+
+    void Update()
+    {
+
+    }
+}
