dda9b8eac987c4546b231edf0d8335553d217944
DTproj <dorian.taradi@gmail.com>
2024-03-27 15:43:07 +0100

    door stuck no longer

--- Assets/Scripts/DoorBehaviour.cs
+++ Assets/Scripts/DoorBehaviour.cs
@@ -9,8 +9,37 @@ public class DoorBehaviour : MonoBehaviour
     private Animator animator;
     private bool isOpen = false;
 
+    public float RequiredPlayerDistance;
+
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Door")
+                {
+                    PlayAnimation();
+                    isOpen = !isOpen;
+                }
+            }
+        }  
+    }
+
+    void PlayAnimation()
+    {
+        animator = transform.parent.gameObject.GetComponent<Animator>();
+
+        if (isOpen)
+        {
+            animator.Play("DoorClose");
+        }
+        else
+        {
+            animator.Play("DoorOpen");
+        }  
     }
 }
