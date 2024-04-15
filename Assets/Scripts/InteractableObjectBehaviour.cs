46cf12e99ff4cb559b60ee854bd2dd1061521cc9
DTproj <dorian.taradi@gmail.com>
2024-04-15 13:35:16 +0200

    stuff

---
+++ Assets/Scripts/InteractableObjectBehaviour.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObjectBehaviour : MonoBehaviour
+{
+    private bool isPlayerClose = false;
+    private Color startClr;
+
+    public PlayerBehaviour PlayerBehaviourScript;
+
+    void Start()
+    {
+        startClr = GetComponent<Renderer>().material.color;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.yellow;
+        CheckDistanceToPlayer();
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = startClr;
+    }
+
+    void CheckDistanceToPlayer()
+    {
+        if (Vector3.Distance(transform.position, PlayerBehaviourScript.transform.position) <= 5)
+        {
+            isPlayerClose = true;
+        }
+        else
+        {
+            isPlayerClose = false;
+        }
+    }
+}
