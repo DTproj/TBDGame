c049131869745facbff5d54ea18f4e4816cd685d
DTproj <dorian.taradi@gmail.com>
2024-04-22 18:15:11 +0200

    dlgggggg

--- Assets/Scripts/DlgManager.cs
+++ Assets/Scripts/DlgManager.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DlgManager : MonoBehaviour
 {
     public Queue<string> sentences;
+
+    public Text nameTxt;
+    public Text dlgTxt;
+
 
     void Start()
     {
@@ -14,5 +19,28 @@ public class DlgManager : MonoBehaviour
     public void StartDlg(Dialogue dlg)
     {
         Debug.Log("the voices in your head are logging this complaint");
+
+        nameTxt.text = dlg.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dlg.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        NextSntc();
+    }
+
+    public void NextSntc()
+    {
+        if(sentences.Count == 0)
+        {
+            Debug.Log("gtfo");
+            return;
+        }
+
+        string sntc = sentences.Dequeue();
+        dlgTxt.text = sntc;
     }
 }
